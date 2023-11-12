86fc25c4292642bd00932a7e976d6219a10691b1
kiki0122 <baihakikiki784@gmail.com>
2023-11-12 22:12:26 +0700

    Menembahkan folder Jobsheet 9

---
+++ Jobsheet_9/ArrayNumbers01.java
@@ -0,0 +1,11 @@
+public class ArrayNumbers01 {
+
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi array dengan tipe data double
+        double[] num = {5.0, 12867.0, 7.5, 2000000.0};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
