aced9e5e58af59ece16cd6fdd451c50f2176ba56
kiki0122 <baihakikiki784@gmail.com>
2023-11-13 23:37:33 +0700

    Menambahkan file LinierSearch01.java

--- Jobsheet_9/ArrayAverageScore01.java
+++ Jobsheet_9/ArrayAverageScore01.java
@@ -1,20 +1,47 @@
 import java.util.Scanner;
+
 public class ArrayAverageScore01 {
     public static void main(String[] args) {
         Scanner input01 = new Scanner(System.in);
 
-        int [] score = new int[10];
-        double total = 0;
-        double average;
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = input01.nextInt();
+
+        int[] score = new int[numberOfStudents];
+        double totalPassed = 0;
+        double totalFailed = 0;
+        double averagePassed;
+        double averageFailed;
+        int passedCount = 0;
+        int failedCount = 0;
 
         for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i + 1) + ": ");
+            System.out.print("Enter the final score " + i + ": ");
             score[i] = input01.nextInt();
-        }for (int i = 0; i < score.length; i++) {
-            total += score[i];
+
+            if (score[i] > 70) {
+                totalPassed += score[i];
+                passedCount++;
+            } else {
+                totalFailed += score[i];
+                failedCount++;
+            }
         }
-        
-        average = total / score.length;
-        System.out.println("The class average score is " + average);
+
+        // Menghitung rata-rata nilai siswa yang lulus
+        if (passedCount > 0) {
+            averagePassed = totalPassed / passedCount;
+            System.out.println("The average score of students who passed is " + averagePassed);
+        } else {
+            System.out.println("No students passed.");
+        }
+
+        // Menghitung rata-rata nilai siswa yang tidak lulus
+        if (failedCount > 0) {
+            averageFailed = totalFailed / failedCount;
+            System.out.println("The average score of students who failed is " + averageFailed);
+        } else {
+            System.out.println("No students failed.");
+        }
     }
 }
