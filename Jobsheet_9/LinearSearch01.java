3b055f455125911d5cb32b88f5524a8365210f45
kiki0122 <baihakikiki784@gmail.com>
2023-11-14 00:00:56 +0700

    Memodifikasi file LinerSearch

--- Jobsheet_9/LinearSearch01.java
+++ Jobsheet_9/LinearSearch01.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class LinearSearch01 {
     public static void main(String[] args) {
-        int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-        int key = 20;
-        int result = 0;
+        Scanner input01 = new Scanner(System.in);
+
+        System.out.print("Enter the number of array elements: ");
+        int numberOfElements = input01.nextInt();
+
+        int[] arrayInt = new int[numberOfElements];
+
+        for (int i = 0; i < numberOfElements; i++) {
+            System.out.print("Enter the array element " + i + ": ");
+            arrayInt[i] = input01.nextInt();
+        }
+
+        System.out.print("Enter the key you want to search for: ");
+        int key = input01.nextInt();
+
+        int result = -1; 
+
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 result = i;
                 break;
             }
         }
-        System.out.println("The key in the array is located at index position " + result);
+
+        if (result != -1) {
+            System.out.println("The key in the array is located at index position " + result);
+        } else {
+            System.out.println("Key not found");
+        }
     }
 }
