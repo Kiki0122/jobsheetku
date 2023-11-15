a5e4c2406e3b37a68d21ec249419c5f19877edfe
kiki0122 <baihakikiki784@gmail.com>
2023-11-16 01:17:59 +0700

    menambahkan folder Job10 dan file Cinema01.java

--- Jobsheet_9/ArrayNumbers01.java
+++ Jobsheet_9/ArrayNumbers01.java
@@ -4,8 +4,9 @@ public class ArrayNumbers01 {
         // Deklarasi dan inisialisasi array dengan tipe data double
         double[] num = {5.0, 12867.0, 7.5, 2000000.0};
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println(num[i]);
-        }
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
     }
 }
