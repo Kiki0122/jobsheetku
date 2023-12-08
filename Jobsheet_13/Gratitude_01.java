94f953761064365438a3d930ed593e4ad249e312
kiki0122 <baihakikiki784@gmail.com>
2023-12-08 21:51:48 +0700

    Mendambahkan file Jobsheet_13

---
+++ Jobsheet_13/Gratitude_01.java
@@ -0,0 +1,9 @@
+public class Gratitude_01 {
+    
+    public static void sayThankyou(){
+        System.out.println("Thankyou \n"+"You inspired");
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}
